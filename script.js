0de8ae21dfdb557364de29d3af2294b3d7241fef
Julius <188955756+4483-s@users.noreply.github.com>
2025-04-06 16:04:40 +0800

    complete most functionalities

--- script.js
+++ script.js
@@ -1,51 +1,75 @@
 const display = document.querySelector(".display >p");
 const btns = document.querySelectorAll("button");
-display.textContent = "0";
 let op1 = "0";
 let op2 = "";
 let operator = "";
+display.textContent = op1;
 btns.forEach((btn) => {
   btn.addEventListener("click", () => {
-    if (btn.textContent.match(/\d/)) {
-      digit(btn.textContent);
+    const cont = btn.textContent;
+    if (cont.match(/[\d.]/)) {
+      digit(cont);
+      display.textContent = op1 + operator + op2;
       return;
     }
-    switch (btn.textContent) {
-      case "+":
-        operator = "+";
-        break;
-      case "-":
-        operator = "-";
-        break;
-      case "*":
-        operator = "*";
-        break;
-      case "/":
-        operator = "/";
-        break;
+    if (cont.match(/[+\-*/]/) && cont !== "+/-") {
+      if (!operator) {
+        operator = cont;
+        display.textContent = op1 + operator + op2;
+        return;
+      }
+      if (op2) {
+        op1 = calculate(op1, operator, op2);
+        op2 = "";
+      }
+      operator = cont;
+      display.textContent = op1 + operator + op2;
+      return;
+    }
+    switch (cont) {
       case "AC":
         op1 = "0";
         op2 = "";
         operator = "";
         break;
+      case "+/-":
+        op1 *= -1;
+        break;
+      case "=":
+        if (op2 && operator) {
+          op1 = calculate(op1, operator, op2);
+          operator = "";
+          op2 = "";
+        }
+        display.textContent = op1 + operator + op2;
+        break;
+      case "%":
+        op1 /= 100;
     }
+    display.textContent = op1 + operator + op2;
   });
 });
 function digit(value) {
+  console.log(value);
   if (op1 === "0") {
-    op1 = value;
+    op1 = value === "." ? op1 + value : value;
+    if (value === "." && op1.includes(".")) {
+      return;
+    }
     return;
   }
   if (!operator) {
+    if (value === "." && op1.includes(".")) {
+      return;
+    }
     op1 += value;
-    display.textContent = op1;
   } else {
+    if (value === "." && op2.includes(".")) {
+      return;
+    }
     op2 += value;
   }
 }
-
-//
-
 function calculate(a, o, b) {
   switch (o) {
     case "*":
@@ -53,7 +77,7 @@ function calculate(a, o, b) {
     case "/":
       return a / b;
     case "+":
-      return a + b;
+      return +a + +b;
     case "-":
       return a - b;
   }
